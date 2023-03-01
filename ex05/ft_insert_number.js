bfb2798b2810ee4f9fba853ea87b547482a47f23
Felix Freyss <felix.freyss@gmail.com>
2023-03-01 20:08:26 +0100

    Push pour l'exo 5

--- ex05/ft_insert_number.js
+++ ex05/ft_insert_number.js
@@ -1,3 +1,15 @@
 /*
 Écrire une fonction qui insert dans un tableau un nombre reçu en paramètre dans l’ordre croissant. Le tableau de référence sera le suivant :
 */
+
+function insererDansTableau(tableau, nombre) {
+    let i = 0;
+    while (i < tableau.length && nombre > tableau[i]) {
+      i++;
+    }
+    tableau.splice(i, 0, nombre);
+    console.log(tableau);
+  }
+
+  const tableauRef = [1, 3, 5, 7];
+insererDansTableau(tableauRef, 4);
