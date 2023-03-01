85e452bfff2e8cbdb503725eb203a2c16c6767aa
Felix Freyss <felix.freyss@gmail.com>
2023-03-01 20:29:04 +0100

    push exo 6 et 7 déso

--- ex06/ft_print_insertion_sort.js
+++ ex06/ft_print_insertion_sort.js
@@ -1,3 +1,17 @@
 /*
 Écrire une fonction qui prend en entrée un tableau d'entiers non trié et qui le trie par insertion. La fonction doit modifier le tableau d'origine et afficher le tableau trié.
 */
+function triInsertion(tab) {
+    for (let i = 1; i < tab.length; i++) {
+      let current = tab[i];
+      let j = i - 1;
+      while (j >= 0 && tab[j] > current) {
+        tab[j + 1] = tab[j];
+        j--;
+      }
+      tab[j + 1] = current;
+    }
+    console.log(tab);
+  
+}let tab = [5, 1, 4, 2, 8];
+  triInsertion(tab); // affiche [1, 2, 4, 5, 8]
--- ex07/ft_print_bubble_sort.js
+++ ex07/ft_print_bubble_sort.js
@@ -1,3 +1,22 @@
 /*
 Écrire une fonction qui prend en entrée un tableau non trié et qui le trie par ordre croissant en utilisant le tri à bulles (bubble sort). La fonction doit modifier le tableau d'origine et afficher le tableau trié.
 */
+function bubbleSort(arr) {
+    let len = arr.length;
+    let swapped;
+    do {
+      swapped = false;
+      for (let i = 0; i < len - 1; i++) {
+        if (arr[i] > arr[i + 1]) {
+          let temp = arr[i];
+          arr[i] = arr[i + 1];
+          arr[i + 1] = temp;
+          swapped = true;
+        }
+      }
+    } while (swapped);
+    console.log(arr);
+  }
+  
+  let myArray = [un, deux, trois, quatre, cinq];
+  bubbleSort(myArray)
